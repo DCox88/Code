349c8a2ade8b86639355643e9bb4172e397a4c92
Grimvalo <38045510+Grimvalo@users.noreply.github.com>
2019-09-06 11:13:55 +0100

    Create data camp Intermediate SQL.sql

---
+++ SQL/data camp Intermediate SQL.sql
@@ -0,0 +1,40 @@
+--filter out all NUll values in case statement
+SELECT
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE
+-- Exclude games not won by Bologna
+	case when hometeam_id = 9857 and home_goal > away_goal then 'Bologna Win'
+		when awayteam_id = 9857 and away_goal > home_goal then 'Bologna Win'
+		end IS NOT null;
+
+--COUNT CASE statements
+SELECT
+	c.name AS country,
+    -- Count matches in each of the 3 seasons
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY c.name;
+
+--calculating percentage with CASE and AVG
+SELECT
+	c.name AS country,
+    -- Round the percentage of tied games to 2 decimal points
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
